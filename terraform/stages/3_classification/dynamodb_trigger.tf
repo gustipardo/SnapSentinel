f1969f61a78fb31286ee2c48383d6cfa4b12068d
gustipardo <gusm2003@gmail.com>
2025-11-28 19:03:36 +1300

    depends_on = [aws_iam_role_policy.event_classifier_policy]. to froce terraform wait.

--- terraform/stages/3_classification/dynamodb_trigger.tf
+++ terraform/stages/3_classification/dynamodb_trigger.tf
@@ -3,4 +3,9 @@ resource "aws_lambda_event_source_mapping" "dynamodb_trigger" {
   function_name     = aws_lambda_function.event_classifier.arn
   starting_position = "LATEST"
   batch_size        = 1
+  enabled           = true
+
+  depends_on = [
+    aws_iam_role_policy.event_classifier_policy
+  ]
 }
